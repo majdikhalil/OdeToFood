2802b7d19e8f558fe70b8d03155574cb754fd74c
majdikhalil <majdi.44@gmail.com>
2018-07-22 22:54:51 -0400

    Added Sql database

---
+++ Services/SqlRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public OdeToFoodDbContext _context { get; private set; }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
